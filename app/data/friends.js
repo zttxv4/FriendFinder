6c5da41b25cc11b1b0c19369ead542e3a80e210f
zttxv4 <zachttaylor00@gmail.com>
2020-02-04 14:28:24 -0600

    update survery.html to handle submit

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friends = [
+    {
+        name: "Ross",
+        photo: "photo",
+        scores: ['5', '3', '2', '5', '2', '5', '1', '1', '1', '1']
+    },
+    
+]
+
+module.exports = friends;
